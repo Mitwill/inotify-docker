08dd40c8ea333a1ace2bef1aee95c90167529bc4
Parth Bhavsar <parth2188.bhavsar@gmail.com>
2025-01-20 13:32:30 +0530

    Updated start.sh to make container alive for scheduled job usage
    we need the container available to sync the filestore to s3 bucket at a defined interval with the help of coolify scheduled job and due to that we are keeping the container alive

--- start.sh
+++ start.sh
@@ -5,5 +5,8 @@ export DIR_PATH=${DIR_PATH:-"/mnt/watcher"}
 
 echo "Initial sync"
 aws s3 sync s3://$BUCKET_NAME $DIR_PATH
-exit 0
+
+# Keep the container alive
+echo "Sync complete. Keeping container alive for further use."
+tail -f /dev/null
 
